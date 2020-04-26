4da9ae0a51e3fb0610ae508ab1e9fb0c5b00821e
kokonstya <kokonstya@gmail.com>
2020-04-26 22:15:51 +0700

    branch for gh-pages

---
+++ precache-manifest.033c453322f66ec9fdf0d5c1cff5b1f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb48c866ff1ec7636b5a818ca99e2b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b1046dc3f99cbd9870",
+    "url": "/static/css/main.0573f8af.chunk.css"
+  },
+  {
+    "revision": "6f993ee542f618711efa",
+    "url": "/static/js/2.639d0bea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.639d0bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b1046dc3f99cbd9870",
+    "url": "/static/js/main.2906ec46.chunk.js"
+  },
+  {
+    "revision": "0ed03701fa80a5ebf92d",
+    "url": "/static/js/runtime-main.b065f19f.js"
+  }
+]);
--- src/components/Error/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Error.css';
-
-const Error = () => {
-    return <div className={'error'}><i className="fa fas fa-bug"></i> <i> Произошла ошибка при загрузке данных
-        =( <hr/> Повторите позже.</i></div>;
-};
-
-export default Error;
